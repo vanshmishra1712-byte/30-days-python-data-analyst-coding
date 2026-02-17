25705786f54efb69ecd8e5b2c3355efb5e50cb16
Vansh <vanshmishra1712@gmail.com>
2026-02-17 09:09:31 +0530

    Day 07: Reverse a string without built-in functions

---
+++ Day07/reverse_string.py
@@ -0,0 +1,9 @@
+# Q7: Reverse a string without using built-in functions
+
+text = "Data Analyst"
+reversed_text = ""
+
+for ch in text:
+    reversed_text = ch + reversed_text
+
+print(reversed_text)
